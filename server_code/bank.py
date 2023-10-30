d9c2e65d1005391f17e30dbcd798a085377afa97
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-10-30 16:11:20 +0000

    Edited bank

--- server_code/bank.py
+++ server_code/bank.py
@@ -119,7 +119,11 @@ def bank_name_exists(bank_name,bank_comp_code):
   else:
     return True
 
+# @anvil.server.callable
+# def bank_get_details(bankcode,bank_comp_code):
+#   row = app_tables.bank.search(bank_code=bankcode,bank_comp_code=bank_comp_code)[0]
+#   return row
+
 @anvil.server.callable
-def bank_get_details(bankcode,bank_comp_code):
-  row = app_tables.bank.search(bank_code=bankcode,bank_comp_code=bank_comp_code)[0]
-  return row
+def bank_get_details():
+  return app_tables.bank.search()
