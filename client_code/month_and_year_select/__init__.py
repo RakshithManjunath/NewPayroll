510e66fbcd4b3cd8f31002d3496a8179377e5217
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-11-07 03:29:52 +0000

    Edited month_and_year_select

--- client_code/month_and_year_select/__init__.py
+++ client_code/month_and_year_select/__init__.py
@@ -19,7 +19,7 @@ class month_and_year_select(month_and_year_selectTemplate):
     self.label_2.text = (gvarb.g_comname+' '+gvarb.g_mode).upper()
 
     self.cur_trans_date = anvil.server.call('cur_trans_date',gvarb.g_comcode)
-
+    
     self.month_names_alphabets = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     self.month_names_numeric = ["01","02","03","04","05","06","07","08","09","10","11","12"]
     self.year_names = ["2023", "2024", "2025", "2026"]
@@ -32,47 +32,25 @@ class month_and_year_select(month_and_year_selectTemplate):
     self.month_db_lbl.text = self.month_names_alphabets[self.month_names_numeric.index(month)]
     self.year_db_lbl.text = self.cur_trans_date[0].year
 
-    self.month_select_dp.items = self.month_names_alphabets
-    self.year_select_dp.items = self.year_names
-
   def submit_btn_click(self, **event_args):
     """This method is called when the button is clicked"""
-    self.format_date_and_save()
-    open_form('menu')
-
-  def month_select_dp_change(self, **event_args):
-    """This method is called when an item is selected"""
-    self.month = self.month_select_dp.selected_value
-    self.encoded_month = self.month_names_numeric[self.month_names_alphabets.index(self.month)]
-    self.month_db_lbl.text = self.month
-
-  def year_select_dp_change(self, **event_args):
-    """This method is called when an item is selected"""
-    self.year = self.year_select_dp.selected_value
-    self.year_db_lbl.text = self.year
-
-  def format_date_and_save(self):
-    if self.year !="" and self.month !="":
-      date_str = "01" + "/" + self.encoded_month + "/" + self.year
-      date_obj = datetime.strptime(date_str, "%d/%m/%Y")
-      gvarb.g_transdate = date_obj.date()
-      #print(gvarb.g_transdate)
-    elif self.year !="":
-      date_str = "01" + "/" + str(self.cur_trans_date[0].month) + "/" + self.year
-      date_obj = datetime.strptime(date_str, "%d/%m/%Y")
-      gvarb.g_transdate = date_obj.date()
-      #print(gvarb.g_transdate)
-    elif self.month != "":
-      date_str = "01" + "/" + self.encoded_month + "/" + str(self.cur_trans_date[0].year)
-      date_obj = datetime.strptime(date_str, "%d/%m/%Y")
-      gvarb.g_transdate = date_obj.date()
-      #print(gvarb.g_transdate)
+    month = self.month_names_numeric[self.month_names_alphabets.index(self.month_db_lbl.text)]
+    if month[0] == '0':
+      month_in_int = int(month[-1])
     else:
-      gvarb.g_transdate = self.cur_trans_date[0]
-      #print(gvarb.g_transdate)
-
-
-
-
-
-    
+      month_in_int = int(month)
+    gvarb.g_transdate = date(self.year_db_lbl.text, month_in_int, 1)
+    print("gvarb transdate", gvarb.g_transdate)
+    open_form('menu')
+     
+  def date_picker_1_change(self, **event_args):
+    """This method is called when the selected date changes"""
+    newdate = self.date_picker_1.date
+    print(newdate)
+    modified_new_date = date(newdate.year,newdate.month, 1)
+    print(modified_new_date)
+    month = str(modified_new_date.month)
+    if modified_new_date.month <=9:
+      month = str(modified_new_date.month).zfill(2)
+    self.month_db_lbl.text = self.month_names_alphabets[self.month_names_numeric.index(str(month))]
+    self.year_db_lbl.text = modified_new_date.year
