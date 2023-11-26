17257176522fc889c678d97a9c5a803a95b0f1d6
systemcomputer.bangalore@gmail.com <systemcomputer.bangalore@gmail.com>
2023-11-26 03:19:02 +0000

    Edited pf_recovery

--- client_code/pf_recovery/__init__.py
+++ client_code/pf_recovery/__init__.py
@@ -24,74 +24,94 @@ class pf_recovery(pf_recoveryTemplate):
     html_template = """
     <!DOCTYPE html>
     <html>
+    
     <head>
         <title>PF Recovery Statement</title>
         <style>
-            body {
-                font-family: Arial, sans-serif;
-                text-align: center; /* Center align the content */
-            }
-            h1 {
-                font-size: 24px;
-            }
-            p {
-                font-size: 16px;
-            }
-            table {
-                width: 80%; /* Adjust the width as needed */
-                margin: 20px auto; /* Center the table horizontally with 20px top and bottom margin */
-                border-collapse: collapse;
-            }
-            th, td {
-                border: 1px solid #000;
-                padding: 8px;
-                text-align: left;
-            }
-        </style>
-    </head>
-    <body>
-    <div class="content">
-        <h1>{{ company_name }}</h1>
-        <h4>{{ addr_line1 }}</h4>
-        <h4>{{ addr_line2 }}</h4>
-        <h4>{{ addr_line3 }}</h4>
-        <h1>{{ report_head }}</h1>
-     </div>
+    body {
+        font-family: Arial, sans-serif;
+        text-align: center;
+        /* Center align the content */
+    }
 
-    <table>
-        <thead>
-            <tr>
-                {% for column in grid_cols %}
-                    <th>{{ column.title }}</th>
-                {% endfor %}
-            </tr>
-        </thead>
-        <tbody>
-            {% for row in grid_rows %}
+    h1 {
+        font-size: 24px;
+    }
+
+    p {
+        font-size: 16px;
+    }
+
+    table {
+        width: 80%;
+        /* Adjust the width as needed */
+        margin: 20px auto;
+        /* Center the table horizontally with 20px top and bottom margin */
+        border-collapse: collapse;
+    }
+
+    th,
+    td {
+        border: 1px solid #000;
+        padding: 8px;
+        text-align: left;
+    }
+
+    .charges-container {
+        text-align: left;
+        /* Adjust margin-top as needed */
+        width: 80%;
+        margin: 20px auto; /* Adjust to match the left margin of the table */
+    }
+</style>
+
+    </head>
+    
+    <body>
+        <div class="content">
+            <h1>{{ company_name }}</h1>
+            <h4>{{ addr_line1 }}</h4>
+            <h4>{{ addr_line2 }}</h4>
+            <h4>{{ addr_line3 }}</h4>
+            <h1>{{ report_head }}</h1>
+        </div>
+    
+        <table>
+            <thead>
                 <tr>
                     {% for column in grid_cols %}
-                        <td>{{ row[column.data_key] }}</td>
+                        <th>{{ column.title }}</th>
                     {% endfor %}
                 </tr>
-            {% endfor %}
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                {% for row in grid_rows %}
+                    <tr>
+                        {% for column in grid_cols %}
+                            <td>{{ row[column.data_key] }}</td>
+                        {% endfor %}
+                    </tr>
+                {% endfor %}
+            </tbody>
+        </table>
 
-    <h1>{{ summary_heading }}</h1>
-    <h1> </h1> 
-    <h4 style="text-align: left;">1 > Total salary for PF [ ₹ ] - {{ report_head }}</h4>
-    <h4 style="text-align: left;">2 > Total salary for FPF [ ₹ ] - {{ report_head }}</h4>
-    <h4 style="text-align: left;">3 > Total employee PF [ ₹ ] - {{ report_head }}</h4>
-    <h4 style="text-align: left;">4 > Total employer PF [ ₹ ] - {{ report_head }}</h4>
-    <h4 style="text-align: left;">5 > Total employee FPF [ ₹ ] - {{ report_head }}</h4>
-    <h4 style="text-align: left;">6 > Total employer FPF [ ₹ ] - {{ report_head }}</h4>
-    <h4 style="text-align: left;">7 > Total admin charges [ ₹ ] - {{ report_head }}</h4>
-    <h4 style="text-align: left;">8 > Total EDLI charges [ ₹ ] - {{ report_head }}</h4>
-    <h4 style="text-align: left;">9 > Total mislenous charges [ ₹ ] - {{ report_head }}</h4>
-    
+        <h1>{{ summary_heading }}</h1>
+        <div class="charges-container">
+            <h4>1 > Total salary for PF [ ₹ ] - {{ report_head }}</h4>
+            <h4>2 > Total salary for FPF [ ₹ ] - {{ report_head }}</h4>
+            <h4>3 > Total employee PF [ ₹ ] - {{ report_head }}</h4>
+            <h4>4 > Total employer PF [ ₹ ] - {{ report_head }}</h4>
+            <h4>5 > Total employee FPF [ ₹ ] - {{ report_head }}</h4>
+            <h4>6 > Total employer FPF [ ₹ ] - {{ report_head }}</h4>
+            <h4>7 > Total admin charges [ ₹ ] - {{ report_head }}</h4>
+            <h4>8 > Total EDLI charges [ ₹ ] - {{ report_head }}</h4>
+            <h4>9 > Total mislenous charges [ ₹ ] - {{ report_head }}</h4>
+        </div>
     
     </body>
+    
     </html>
+
     """
 
     self.html_content = anvil.server.call('pf_recovery_html',html_template,
